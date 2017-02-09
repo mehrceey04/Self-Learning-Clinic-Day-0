7175d46c534660d41a8fd1ae74b7dac3ef54f7fb
Halimat Mercy Oseni <mehrceey04@gmail.com>
2017-02-09 05:52:03 +0100

    Added codes to pass tests for fizzBuzz

---
+++ app/fizzBuzz.js
@@ -0,0 +1,16 @@
+module.exports = {
+  fizzBuzz : function(number) {
+    if(number % 3 === 0 && number % 5 === 0) {
+      return "FizzBuzz";
+    }
+    if(number % 3 === 0) {
+      return "Fizz";
+    }
+    if(number % 5 === 0) {
+      return "Buzz";
+    }
+    else{
+      return number;
+    }
+  }
+}
